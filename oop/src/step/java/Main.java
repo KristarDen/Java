3081aaf7877fe6a37f4fa63cef4b18ae2e3ada34
KristarDen <denis216051@gmail.com>
2021-09-20 13:21:26 +0300

    File creator

--- oop/src/step/java/Main.java
+++ oop/src/step/java/Main.java
@@ -1,5 +1,6 @@
 package step.java;
 
+import step.java.fs.CreateFiles;
 import step.java.library.Book;
 import step.java.library.Hologram;
 import step.java.library.Journal;
@@ -8,10 +9,15 @@ import step.java.library.Library;
 public class Main {
 
     public static void main(String[] args) {
-        Library lib = new Library() ;
+        /*Library lib = new Library() ;
         lib.add( new Journal( "Quantum Mechanics", "2021, 1" ) ) ;
         lib.add( new Book( "Martin Eden", "Jack London" ) ) ;
         lib.add( new Hologram( "Pectoral" ) ) ;
-        lib.print() ;
+        lib.print() ;*/
+
+        CreateFiles creator = new CreateFiles();
+        //creator.showDir();
+        creator.createBook();
+        creator.createJournal();
     }
 }
